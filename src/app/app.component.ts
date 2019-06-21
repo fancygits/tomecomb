47a71d48ccf81fffb5d5d19d6fdac67532813462
James Luke Johnson <jjohn144@my.westga.edu>
2019-06-20 23:42:07 -0500

    Added simple API get method.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tome Comb';
+  books: any = {};
+  private apiKey: string;
+  private apiUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.apiKey = '***REMOVED***';
+    this.apiUrl = 'https://www.googleapis.com/books/v1/volumes';
+  }
+
+  searchBooks(searchterm: string) {
+    this.books = this.http.get(this.apiUrl + '?q=' + searchterm + '&key=' + this.apiKey);
+  }
 }
