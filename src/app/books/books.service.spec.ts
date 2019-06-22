252fd46746935d560702a784a7f0a3554886620a
James Luke Johnson <jjohn144@my.westga.edu>
2019-06-22 16:24:14 -0500

    Modified query and set up testbed for book.service

--- src/app/books/books.service.spec.ts
+++ src/app/books/books.service.spec.ts
@@ -1,12 +1,38 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, getTestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { BooksService } from './books.service';
 
 describe('BooksService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let injector: TestBed;
+  let service: BooksService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [BooksService]
+    });
+    injector = getTestBed();
+    service = injector.get(BooksService);
+    httpMock = injector.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
 
   it('should be created', () => {
-    const service: BooksService = TestBed.get(BooksService);
+    service = TestBed.get(BooksService);
     expect(service).toBeTruthy();
+  });
+
+  it('should get a list of books about harry potter', () => {
+    service = TestBed.get(BooksService);
+    service.getBookList('harry potter', 0).subscribe(books => {
+      expect(books.items).toBe(true);
+      expect(books.items.length).toBe(10);
+    })
+    expect(service.getBookList('harry potter', 0)).toBe('json');
   });
 });
