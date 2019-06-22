0a296740b9236d34a176713ecb9d027feb95fcb2
James Luke Johnson <jjohn144@my.westga.edu>
2019-06-22 16:24:14 -0500

    Modified query and set up testbed for book.service

--- src/app/books/books.service.ts
+++ src/app/books/books.service.ts
@@ -3,13 +3,13 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class BooksService {
-  private apiKey = '***REMOVED***';
-  private apiUrl = 'https://www.googleapis.com/books/v1/volumes';
+  readonly apiKey = '***REMOVED***';
+  readonly apiUrl = 'https://www.googleapis.com/books/v1/volumes';
 
   constructor(private http: HttpClient) { }
 
   getBookList(searchterm: string, index: number) {
-    const query = this.apiUrl + '?q=' + searchterm + '&&printType=books&maxResults=10&startIndex=' + index + '&key=' + this.apiKey;
+    const query = this.apiUrl + '?q=' + searchterm + '&printType=books&maxResults=10&startIndex=' + index + '&key=' + this.apiKey;
     return this.http.get(query);
   }
 
