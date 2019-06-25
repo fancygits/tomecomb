738139e393365f95090b97a611fa9597c998f10f
James Luke Johnson <jjohn144@my.westga.edu>
2019-06-24 22:48:04 -0500

    Modified search to happen automatically.
    Using DebouceTime, books are searched after 3 keystrokes.
    Added hiddenSearch for form submit on enter.
    Tested component using spies and mockBook.

---
+++ src/app/books/book.mock.ts
@@ -0,0 +1,32 @@
+export const MOCKBOOK = 
+{
+ "kind": "books#volumes",
+ "totalItems": 2405,
+ "items": [
+  {
+   "kind": "books#volume",
+   "id": "tcSMCwAAQBAJ",
+   "volumeInfo": {
+    "title": "Harry Potter and the Cursed Child – Parts One and Two (Special Rehearsal Edition)",
+    "authors": [
+     "J.K. Rowling",
+     "John Tiffany",
+     "Jack Thorne"
+    ],
+    "pageCount": 320,
+    "categories": [
+     "Drama"
+    ],
+    "imageLinks": {
+     "smallThumbnail": "http://books.google.com/books/content?id=tcSMCwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+     "thumbnail": "http://books.google.com/books/content?id=tcSMCwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+    },
+    "previewLink": "http://books.google.com/books?id=tcSMCwAAQBAJ&printsec=frontcover&dq=harry+potter&hl=&as_pt=BOOKS&cd=1&source=gbs_api",
+    "infoLink": "http://books.google.com/books?id=tcSMCwAAQBAJ&dq=harry+potter&hl=&as_pt=BOOKS&source=gbs_api",
+    "canonicalVolumeLink": "https://books.google.com/books/about/Harry_Potter_and_the_Cursed_Child_Parts.html?hl=&id=tcSMCwAAQBAJ"
+   },
+   "searchInfo": {
+    "textSnippet": "Based on an original new story by J.K. Rowling, Jack Thorne and John Tiffany, a new play by Jack Thorne, Harry Potter and the Cursed Child is the eighth story in the Harry Potter series and the first official Harry Potter story to be ..."
+   }
+  }]
+};
