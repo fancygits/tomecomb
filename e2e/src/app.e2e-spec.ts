4bf14fd77f145a4f1de3ecf60c62ceeba3e5a706
James Luke Johnson <jjohn144@my.westga.edu>
2019-07-02 01:56:55 -0500

    End to end testing of navigation and searching.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, logging, element, by } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -10,7 +10,43 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to tomecomb!');
+    expect(browser.getTitle()).toEqual('Tome Comb :: Book Search');
+  });
+
+  it('should only have an image and search bar (no books)', () => {
+    expect(page.getLogo().getAttribute('alt')).toEqual('Tome Comb');
+    expect(element(by.name('searchterm'))).toBeTruthy();
+  });
+
+  it('should search for books after 3 keystrokes and 1 second', () => {
+    page.search('ha');
+    expect(page.getBooks().count()).toEqual(0);
+    page.search('r');
+    expect(page.getBooks().count()).toEqual(12);
+  });
+
+  it('should load a page of books', () => {
+    page.getSearchbar().clear();
+    page.search('tron');
+    expect(page.getBooks().count()).toEqual(12);
+    page.getBooks().each(function(book, index) {
+      let text = book.getText();
+      expect(book.getText()).toMatch(/tron/i, "Tron wasn't found in book # " + index);
+    })
+  });
+
+  it('should navigate between pages of books', () => {
+    page.getSearchbar().clear();
+    page.search('Angular Testing');
+    expect(page.getPageRange()).toContain('1 - 12 of ');
+    page.nextPage();
+    expect(page.getPageRange()).toContain('13 - 24 of ');
+    page.nextPage();
+    expect(page.getPageRange()).toContain('25 - 36 of ');
+    page.previousPage();
+    expect(page.getPageRange()).toContain('13 - 24 of ');
+    page.previousPage();
+    expect(page.getPageRange()).toContain('1 - 12 of ');
   });
 
   afterEach(async () => {
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -5,7 +5,34 @@ export class AppPage {
     return browser.get(browser.baseUrl) as Promise<any>;
   }
 
-  getTitleText() {
-    return element(by.css('app-root h1')).getText() as Promise<string>;
+  getLogo() {
+    return element(by.css("img[src*='assets/img/tome_comb_logo.svg']"));
+  }
+
+  getSearchbar() {
+    return element(by.name('searchterm'));
+  }
+
+  search(term: string) {
+    element(by.name('searchterm')).sendKeys(term);
+    browser.sleep(5000);
+  }
+
+  getBooks() {
+    return element.all(by.css('mat-card'));
+  }
+
+  getPageRange() {
+    return element(by.css('.mat-paginator-range-label')).getText();
+  }
+
+  nextPage() {
+    element(by.css('.mat-paginator-navigation-next')).click();
+    browser.sleep(5000);
+  }
+
+  previousPage() {
+    element(by.css('.mat-paginator-navigation-previous')).click();
+    browser.sleep(5000);
   }
 }
